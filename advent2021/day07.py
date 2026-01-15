08456143114324d04860ec9617ec2ecc7e0b4fd3
alanese <43454736+alanese@users.noreply.github.com>
2026-01-15 12:50:37 -0500

    mark parts

--- advent2021/day07.py
+++ advent2021/day07.py
@@ -8,11 +8,13 @@ def cost_p2(crabs: list[int], point: int) -> int:
 with open("input-07.txt") as f:
     data: list[int] = [int(x) for x in f.read().strip().split(",")]
 
+#Part 1
 min_cost: int = sum(data)
 for x in range(max(data)+1):
     min_cost = min(min_cost, sum(abs(x-point) for point in data))
 print(min_cost)
 
+#Part 2
 min_cost = cost_p2(data, 0)
 for point in range(1, max(data)+1):
     min_cost = min(min_cost, cost_p2(data, point))
